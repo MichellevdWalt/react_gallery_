3625d24342414a858b330a5fab9272b7816eea02
MichellevdWalt <michellevdwalt13@gmail.com>
2020-07-22 20:10:00 +0200

    Updates

---
+++ precache-manifest.c4b17746f817306ce6e222fc649cec6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2057b6c5ac8318b3661994a3c68fc6",
+    "url": "/react_gallery/index.html"
+  },
+  {
+    "revision": "815bfd2bd4eaeda432fe",
+    "url": "/react_gallery/static/css/main.36cd6399.chunk.css"
+  },
+  {
+    "revision": "70f1ba0cc23a320c2f7b",
+    "url": "/react_gallery/static/js/2.85205519.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_gallery/static/js/2.85205519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815bfd2bd4eaeda432fe",
+    "url": "/react_gallery/static/js/main.31395dbf.chunk.js"
+  },
+  {
+    "revision": "4171937857c25f7d48f1",
+    "url": "/react_gallery/static/js/runtime-main.62dbb8a7.js"
+  },
+  {
+    "revision": "f6a7184a76449280c42633f192cd95cf",
+    "url": "/react_gallery/static/media/corner_left.f6a7184a.png"
+  },
+  {
+    "revision": "5877eb53ed83bffcb56a19d880c37124",
+    "url": "/react_gallery/static/media/corner_right.5877eb53.png"
+  },
+  {
+    "revision": "83a2d7b14d4dda6ddd80e60bbda19978",
+    "url": "/react_gallery/static/media/unicorn1.83a2d7b1.jpg"
+  }
+]);
